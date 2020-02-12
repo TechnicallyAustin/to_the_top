f7447516d466c42eff8680164e084976685499d7
Technicallyaustin <Technicallyaustin@gmail.com>
2020-02-11 17:51:49 -0800

    R

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -1,8 +1,11 @@
 require 'pry'
-
+require 'nokogiri'
+require 'open-uri'
 class Scrape
 attr_accessor :artist, :position, :song, :featuring
 @@all = []
 doc = Nokogiri::HTML(open("https://www.billboard.com/charts/hot-100"))
+binding.pry
+end
 
 
