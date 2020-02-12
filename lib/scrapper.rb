d22f156ce372b8a99cf2d34a2478f148c150f8b3
Technicallyaustin <Technicallyaustin@gmail.com>
2020-02-11 17:52:09 -0800

    R

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -6,6 +6,22 @@ attr_accessor :artist, :position, :song, :featuring
 @@all = []
 doc = Nokogiri::HTML(open("https://www.billboard.com/charts/hot-100"))
 binding.pry
+def artist
+    Nokogiri::HTML(doc.css())
+
+end
+
+def song
+    #
+end
+
+def position
+end
+
+
+
+
+
 end
 
 
