c4674de57089aa252c29fb506dd75a86f0323adb
Technicallyaustin <Technicallyaustin@gmail.com>
2020-02-12 12:12:50 -0800

    added CSS selectors to scrapper

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -12,13 +12,18 @@ def artist
 end
 
 def song
-    #
+    # 
 end
 
 def position
 end
 
-
+# rank: "div.ye-chart-item_rank"
+# title: "div.ye-chart-item_title"
+# artist: "div.ye-chart-item_artist"
+# peak: "span.decade-end-chart-item_peak-info-rank"
+# peak date: "span.decade-end-chart-item-info_date"
+# website "https://www.billboard.com/charts/decade-end/hot-100"
 
 
 
