8c6a13cb0d25a4e35a5cd40d0982eae69d767ca4
Technicallyaustin <Technicallyaustin@gmail.com>
2020-02-12 12:36:20 -0800

    added and tested css selectors

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -4,7 +4,8 @@ require 'open-uri'
 class Scrape
 attr_accessor :artist, :position, :song, :featuring
 @@all = []
-doc = Nokogiri::HTML(open("https://www.billboard.com/charts/hot-100"))
+doc = Nokogiri::HTML(open("https://www.billboard.com/charts/decade-end/hot-100"))
+
 binding.pry
 def artist
     Nokogiri::HTML(doc.css())
@@ -18,11 +19,14 @@ end
 def position
 end
 
-# rank: "div.ye-chart-item_rank"
-# title: "div.ye-chart-item_title"
-# artist: "div.ye-chart-item_artist"
-# peak: "span.decade-end-chart-item_peak-info-rank"
-# peak date: "span.decade-end-chart-item-info_date"
+#Nokogiri CSS Selectors
+# ------------------------
+# rank:  doc.css("div.ye-chart-item__rank").first.text
+# title: doc.css("div.ye-chart-item__title").first.text
+# artist: doc.css("div.ye-chart-item__artist").first.text
+# peak: doc.css("span.decade-end-chart-item__peak-info-rank").first.text
+# peak date: doc.css("span.decade-end-chart-item__peak-info-date").first.text
+# entire chart: doc.css("div.chart-details__item-list").first.text
 # website "https://www.billboard.com/charts/decade-end/hot-100"
 
 
